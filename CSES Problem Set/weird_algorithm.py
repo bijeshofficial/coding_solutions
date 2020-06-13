7f9331328a391834fda58f834d73e1280664a591
bijeshofficial <bijeshr.kunwar@gmail.com>
2020-06-13 21:29:03 +0545

    added CSES

---
+++ CSES Problem Set/weird_algorithm.py
@@ -0,0 +1,15 @@
+n = int(input())
+while n != 1:
+    print(int(n),end= ' ')
+    if n % 2== 0:
+        n /= 2
+    else:
+        n *= 3
+        n += 1
+print(1)
+	
+#2020-06-13 
+# 18:37:47 
+# Python3 
+# 0.05s 
+# 78 ch.		
